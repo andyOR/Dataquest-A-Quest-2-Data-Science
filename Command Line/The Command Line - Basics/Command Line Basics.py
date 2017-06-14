3ce0fb598f1451eef416caa2938e04cb094e203f
andyOR <aniketskpl19@gmail.com>
2017-06-14 18:33:44 -0400

    Started Command Line Basics

---
+++ Command Line/The Command Line - Basics/Command Line Basics.py
@@ -0,0 +1,68 @@
+## In this script, we will learn basics of command line interface
+## A command line interface lets us switch between folders and launch programs by typing commands.
+## A command line interface is often faster and more powerful than a GUI for programming tasks
+
+
+## Basic commands
+
+pwd ## to print working diirectory
+cd /## to move to home or root directory
+
+
+## Absolute Vs. Relative Paths
+
+
+## Getting Back To The Dq Folder
+
+cd dq # if we are home folder containing dq
+
+
+## Understanding Users
+
+whoami # current user
+
+
+## The Home Directory
+
+cd ~
+
+
+## Making A New Directory
+
+mkdir ## make directory in current folder
+mkdir /home/dq/test # directory with absolute path
+
+
+## Using Command Options To Modify Behavior
+
+mkdir -v test2 # command option to show output expressions
+# output - mkdir: created directory 'test2'
+
+
+## Reviewing Available Command Options
+
+mkdir --help
+
+
+## Listing The Contents Of A Directory
+
+ls -1
+
+
+## Removing A Directory
+
+rmdir test2
+
+
+
+## END
+
+
+
+
+
+
+
+
+
+
