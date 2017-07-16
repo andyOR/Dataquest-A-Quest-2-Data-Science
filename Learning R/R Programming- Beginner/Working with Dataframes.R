a88fb84507883c79288cad8c2ae8d4beaf2a0129
andyOR <aniketskpl19@gmail.com>
2017-07-16 11:00:44 -0400

    Started working with Dataframes in R

---
+++ Learning R/R Programming- Beginner/Working with Dataframes.R
@@ -0,0 +1,32 @@
+## Introduction To Functions
+
+add <- function(a, b){
+  d <- a + b
+  return(d)
+}
+
+
+## Calling Functions
+
+# Define the add function
+add <- function(a, b){
+  return(a + b)
+}
+
+# Call the add function with the arguments 1 and 2
+print(add(1, 2))
+d <- add(5,10)
+
+
+## Defining A Function
+
+# Enter your code here
+subtract <- function(a,b) {
+  p = a-b
+  return(p)
+}
+
+d <- subtract(50,10)
+
+
+## 
