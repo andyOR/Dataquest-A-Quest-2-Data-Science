f44a0e6059dd3cbf2f3aed9f8d21b3f1a1074ad8
andyOR <aniketskpl19@gmail.com>
2017-07-16 21:03:42 -0400

    Started Calculus in ML

---
+++ Machine Learning/Calculus For Machine Learning/Understanding Linear and Nonlinear Functions.py
@@ -0,0 +1,17 @@
+##  In this course, we'll explore the different calculus concepts necessary to build up in Machine Learning models
+
+## Why Learn Calculus?
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0,3,301)
+y = -(x ** 2) + 3*x -1
+
+plt.plot(x,y)
+
+
+## Linear Function
+
+
+## 
