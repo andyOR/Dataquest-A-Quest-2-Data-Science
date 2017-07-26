94628407f965d82fe64a361e1c945b20554a10f1
andyOR <aniketskpl19@gmail.com>
2017-07-26 15:46:05 -0400

    Added ML in Python

---
+++ Machine Learning/Machine Learning in Python/Logistic regression.py
@@ -0,0 +1,73 @@
+## In this script, we'll explore how to build a predictive model for these types of problems, which are known as classification problems
+
+
+## Classification
+
+
+## Introduction To The Data
+
+import pandas as pd
+import matplotlib.pyplot as plt
+admissions = pd.read_csv("admissions.csv")
+plt.scatter(admissions['gpa'], admissions['admit'])
+plt.show()
+
+
+## Logistic Regression
+
+## Logit Function
+
+import numpy as np
+
+# Logit Function
+def logit(x):
+    # np.exp(x) raises x to the exponential power, ie e^x. e ~= 2.71828
+    return np.exp(x)  / (1 + np.exp(x)) 
+    
+# Generate 50 real values, evenly spaced, between -6 and 6.
+x = np.linspace(-6,6,50, dtype=float)
+
+# Transform each number in t using the logit function.
+y = logit(x)
+
+# Plot the resulting data.
+plt.plot(x, y)
+plt.ylabel("Probability")
+plt.show()
+
+
+## Training A Logistic Regression Model
+
+from sklearn.linear_model import LinearRegression
+linear_model = LinearRegression()
+linear_model.fit(admissions[["gpa"]], admissions["admit"])
+
+from sklearn.linear_model import LogisticRegression
+logistic_model = LogisticRegression()
+logistic_model.fit(admissions[["gpa"]], admissions['admit'])
+
+
+## Plotting Probabilities
+
+logistic_model = LogisticRegression()
+logistic_model.fit(admissions[["gpa"]], admissions["admit"])
+
+pred_probs = logistic_model.predict_proba(admissions[["gpa"]])
+
+plt.scatter(admissions["gpa"], pred_probs[:,1])
+plt.xlabel('GPA')
+plt.ylabel('Prob of Admit students')
+plt.title('Logistic Regression plot')
+plt.show()
+
+
+## Predict Labels
+
+logistic_model = LogisticRegression()
+logistic_model.fit(admissions[["gpa"]], admissions["admit"])
+
+fitted_labels = logistic_model.predict(admissions[["gpa"]])
+print(fitted_labels)
+
+
+## END
