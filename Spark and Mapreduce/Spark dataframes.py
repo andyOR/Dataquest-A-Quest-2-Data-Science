b1f49d409c066d22dade94319b7969831dc601d3
andyOR <aniketskpl19@gmail.com>
2017-09-23 20:41:20 -0400

    Added Spark dataframes

--- Spark and Mapreduce/Spark dataframes.py
+++ Spark and Mapreduce/Spark dataframes.py
@@ -32,4 +32,39 @@ df = sqlCtx.read.json("census_2010.json")
 df.printSchema()
 
 
-## 
+## Pandas vs. Spark Dataframes
+
+df.show()
+
+
+## Row objects
+
+first_five = df.head(5)
+for r in first_five:
+    print(r.age)
+
+
+## Selecting columns
+
+df[['age']].show()
+df[['age', 'males', 'females']].show()
+
+
+## Filtering rows
+
+five_plus = df[df['age']>5]
+five_plus.show()
+
+
+## Filtering columns
+
+first_row = df[df['females']< df['males']]
+first_row.show(20)
+
+
+## Converting Spark dataframes to Pandas dataframes
+
+pandas_df = df.toPandas()
+pandas_df['total'].hist()
+
+## END
