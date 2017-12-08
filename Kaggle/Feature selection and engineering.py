a0fd0394bfb5600228a290dd16262bb7d86a19bb
andyOR <aniketskpl19@gmail.com>
2017-12-08 18:49:14 -0500

    Kaggle feature selection and engineering

--- Kaggle/Feature selection and engineering.py
+++ Kaggle/Feature selection and engineering.py
@@ -82,3 +82,176 @@ lr = LogisticRegression()
 scores = cross_val_score(lr, train[columns], train['Survived'], cv = 10)
 accuracy = np.mean(scores)
 print(accuracy)
+
+
+## Submitting improved model
+
+columns = ['Age_categories_Infant', 'SibSp_scaled', 'Sex_female', 'Sex_male',
+       'Pclass_1', 'Pclass_3', 'Age_categories_Senior', 'Parch_scaled']
+
+all_X = train[columns]
+all_y = train['Survived']
+lr = LogisticRegression()
+lr.fit(all_X, all_y)
+holdout_predictions = lr.predict(holdout[columns])
+
+submission = pd.DataFrame({
+    "PassengerId":holdout["PassengerId"],
+    "Survived":holdout_predictions
+})
+
+submission.to_csv("submission_1.csv", index=False)
+
+
+## Engineering a new feature using binning
+
+def process_age(df,cut_points,label_names):
+    df["Age"] = df["Age"].fillna(-0.5)
+    df["Age_categories"] = pd.cut(df["Age"],cut_points,labels=label_names)
+    return df
+
+def process_fare(df,cut_points,label_names):
+    df["Fare_categories"] = pd.cut(df["Fare"],cut_points,labels=label_names)
+    return df
+
+bins = [0,12,50,100, 1000]
+column_names = ["0-12","12-50","50-100","100+"]
+
+train = process_fare(train, bins, column_names)
+holdout = process_fare(holdout, bins, column_names)
+
+train = create_dummies(train, "Fare_categories")
+holdout = create_dummies(holdout, "Fare_categories")
+
+
+## Engineering features from text column
+
+titles = {
+    "Mr" :         "Mr",
+    "Mme":         "Mrs",
+    "Ms":          "Mrs",
+    "Mrs" :        "Mrs",
+    "Master" :     "Master",
+    "Mlle":        "Miss",
+    "Miss" :       "Miss",
+    "Capt":        "Officer",
+    "Col":         "Officer",
+    "Major":       "Officer",
+    "Dr":          "Officer",
+    "Rev":         "Officer",
+    "Jonkheer":    "Royalty",
+    "Don":         "Royalty",
+    "Sir" :        "Royalty",
+    "Countess":    "Royalty",
+    "Dona":        "Royalty",
+    "Lady" :       "Royalty"
+}
+
+extracted_titles = train["Name"].str.extract(' ([A-Za-z]+)\.',expand=False)
+train["Title"] = extracted_titles.map(titles)
+
+extracted_titles = holdout["Name"].str.extract(' ([A-Za-z]+)\.', expand=False)
+holdout['Title'] = extracted_titles.map(titles)
+
+train['Cabin_type'] = train["Cabin"].str[0]
+train['Cabin_type'] = train['Cabin_type'].fillna("Unknown")
+
+holdout['Cabin_type'] = holdout["Cabin"].str[0]
+holdout['Cabin_type'] = holdout['Cabin_type'].fillna("Unknown")
+
+train = create_dummies(train, "Title")
+train = create_dummies(train, "Cabin_type")
+
+holdout = create_dummies(holdout, "Title")
+holdout = create_dummies(holdout, "Cabin_type")
+
+
+## Finding correlated features
+
+import numpy as np
+import seaborn as sns
+
+def plot_correlation_heatmap(df):
+    corr = df.corr()
+    
+    sns.set(style="white")
+    mask = np.zeros_like(corr, dtype=np.bool)
+    mask[np.triu_indices_from(mask)] = True
+
+    f, ax = plt.subplots(figsize=(11, 9))
+    cmap = sns.diverging_palette(220, 10, as_cmap=True)
+
+
+    sns.heatmap(corr, mask=mask, cmap=cmap, vmax=.3, center=0,
+            square=True, linewidths=.5, cbar_kws={"shrink": .5})
+    plt.show()
+
+columns = ['Age_categories_Missing', 'Age_categories_Infant',
+       'Age_categories_Child', 'Age_categories_Teenager',
+       'Age_categories_Young Adult', 'Age_categories_Adult',
+       'Age_categories_Senior', 'Pclass_1', 'Pclass_2', 'Pclass_3',
+       'Sex_female', 'Sex_male', 'Embarked_C', 'Embarked_Q', 'Embarked_S',
+       'SibSp_scaled', 'Parch_scaled', 'Fare_categories_0-12',
+       'Fare_categories_12-50','Fare_categories_50-100', 'Fare_categories_100+',
+       'Title_Master', 'Title_Miss', 'Title_Mr','Title_Mrs', 'Title_Officer',
+       'Title_Royalty', 'Cabin_type_A','Cabin_type_B', 'Cabin_type_C', 'Cabin_type_D',
+       'Cabin_type_E','Cabin_type_F', 'Cabin_type_G', 'Cabin_type_T', 'Cabin_type_Unknown']
+
+plot_correlation_heatmap(train[columns])
+
+
+## Final feature using RFECV
+
+from sklearn.feature_selection import RFECV
+
+columns = ['Age_categories_Missing', 'Age_categories_Infant',
+       'Age_categories_Child', 'Age_categories_Young Adult',
+       'Age_categories_Adult', 'Age_categories_Senior', 'Pclass_1', 'Pclass_3',
+       'Embarked_C', 'Embarked_Q', 'Embarked_S', 'SibSp_scaled',
+       'Parch_scaled', 'Fare_categories_0-12', 'Fare_categories_50-100',
+       'Fare_categories_100+', 'Title_Miss', 'Title_Mr', 'Title_Mrs',
+       'Title_Officer', 'Title_Royalty', 'Cabin_type_B', 'Cabin_type_C',
+       'Cabin_type_D', 'Cabin_type_E', 'Cabin_type_F', 'Cabin_type_G',
+       'Cabin_type_T', 'Cabin_type_Unknown']
+
+all_X = train[columns]
+all_y = train["Survived"]
+
+lr = LogisticRegression()
+selector = RFECV(lr,cv=10)
+selector.fit(all_X,all_y)
+optimized_columns = all_X.columns[selector.support_]
+
+
+## Traing the model using optimized column
+
+from sklearn.model_selection import cross_val_score
+import numpy as np
+all_X = train[optimized_columns]
+all_y = train["Survived"]
+
+lr = LogisticRegression()
+scores = cross_val_score(lr, all_X, all_y, cv = 10)
+accuracy = np.mean(scores)
+print(accuracy)
+
+
+## More improved submission
+
+all_X = train[optimized_columns]
+all_y = train['Survived']
+lr = LogisticRegression()
+lr.fit(all_X, all_y)
+holdout_predictions = lr.predict(holdout[optimized_columns])
+
+submission = pd.DataFrame({
+    "PassengerId":holdout["PassengerId"],
+    "Survived":holdout_predictions
+})
+
+submission.to_csv("submission_2.csv", index=False)
+
+
+##End
+
+ 
