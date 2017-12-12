d0055729113c31f5f0a3b42cdbd0e3a38cae9886
andyOR <aniketskpl19@gmail.com>
2017-12-12 02:27:13 -0500

    Machine learning project -making predictions

---
+++ Machine Learning/Machine Learning Project/MLP- Making predictions.py
@@ -0,0 +1,129 @@
+## In this cript, we will make predictions from the features we created in last
+## two scripts of data cleaning and preparing features
+
+
+## Recap
+
+import pandas as pd
+loans = pd.read_csv("cleaned_loans_2007.csv")
+print(loans.info())
+
+
+## Picking an error metric
+
+import pandas as pd
+
+tn = (predictions == 0) & (loans["loan_status"] == 0)
+tp = (predictions == 1) & (loans["loan_status"] == 1)
+fn = (predictions == 0) & (loans["loan_status"] == 1)
+fp = (predictions == 1) & (loans["loan_status"] == 0)
+
+
+## Class imbalance
+
+import pandas as pd
+import numpy as np
+
+predictions = pd.Series(numpy.ones(loans.shape[0]))
+# False positives.
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / (tp + fn)
+fpr = fp / (fp + tn)
+
+print(tpr)
+print(fpr)
+
+
+## Logistic Regression
+
+from sklearn.linear_model import LogisticRegression
+lr = LogisticRegression()
+
+features = loans.columns.tolist()
+features.remove('loan_status')
+target  = loans['loan_status']
+lr.fit(loans[features],target)
+predictions = lr.predict(loans[features])
+
+
+## Cross validation
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_predict, KFold
+lr = LogisticRegression()
+kf = KFold(features.shape[0], random_state=1)
+
+predictions = cross_val_predict(lr, features, target, cv = kf)
+predictions = pd.Series(predictions)
+
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / (tp + fn)
+fpr = fp / (fp + tn)
+
+print(tpr)
+print(fpr)
+
+
+## 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
