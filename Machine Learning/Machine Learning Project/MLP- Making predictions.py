b14432257da4bc96967c379925f5477bf4b818d0
andyOR <aniketskpl19@gmail.com>
2017-12-12 18:47:37 -0500

    Advanced python - exeption handling

--- Machine Learning/Machine Learning Project/MLP- Making predictions.py
+++ Machine Learning/Machine Learning Project/MLP- Making predictions.py
@@ -94,7 +94,114 @@ print(tpr)
 print(fpr)
 
 
-## 
+## Penalizing the classifier
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_predict
+
+lr = LogisticRegression(class_weight = 'balanced')
+kf = KFold(features.shape[0], random_state = 1)
+
+predictions = cross_val_predict(lr, features, target, cv=kf)
+predictions = pd.Series(predictions)
+
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / (tp + fn)
+fpr = fp / (fp + tn)
+
+print(tpr)
+print(fpr)
+
+
+## Manual Penalties
+
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import cross_val_predict
+
+penalty = {
+    0: 10,
+    1: 1
+}
+lr = LogisticRegression(class_weight=penalty)
+kf = KFold(features.shape[0], random_state = 1)
+
+predictions = cross_val_predict(lr, features, target, cv=kf)
+predictions = pd.Series(predictions)
+
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / (tp + fn)
+fpr = fp / (fp + tn)
+
+print(tpr)
+print(fpr)
+
+
+## Rando Forests
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.cross_validation import cross_val_predict
+
+lr = RandomForestClassifier(class_weight = 'balanced')
+kf = KFold(features.shape[0], random_state = 1)
+
+predictions = cross_val_predict(lr, features, target, cv=kf)
+predictions = pd.Series(predictions)
+
+fp_filter = (predictions == 1) & (loans["loan_status"] == 0)
+fp = len(predictions[fp_filter])
+
+# True positives.
+tp_filter = (predictions == 1) & (loans["loan_status"] == 1)
+tp = len(predictions[tp_filter])
+
+# False negatives.
+fn_filter = (predictions == 0) & (loans["loan_status"] == 1)
+fn = len(predictions[fn_filter])
+
+# True negatives
+tn_filter = (predictions == 0) & (loans["loan_status"] == 0)
+tn = len(predictions[tn_filter])
+
+# Rates
+tpr = tp / (tp + fn)
+fpr = fp / (fp + tn)
+
+print(tpr)
+print(fpr)
+
+
+## END
+
 
 
 
